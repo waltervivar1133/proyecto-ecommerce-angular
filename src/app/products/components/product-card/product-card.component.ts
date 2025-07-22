7e5d60a42bfa9018cff6e866fbaa3ae30b9e4496
walter vivar <waltervivar1133@gmail.com>
2025-07-21 22:58:01 -0500

    feat: add components home and product card

---
+++ src/app/products/components/product-card/product-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-card',
+  imports: [],
+  templateUrl: './product-card.component.html',
+})
+export class ProductCardComponent { }
